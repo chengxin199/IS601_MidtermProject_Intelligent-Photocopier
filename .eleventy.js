be943c858f609e9911786cbf2793970434c51254
chengxin199 <chengxin199@gmail.com>
2025-12-04 16:49:06 -0500

    Add Netlify deployment config and update BrowserSync settings

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,34 @@ module.exports = function (eleventyConfig) {
     // Configure syntax highlighting
     eleventyConfig.addPlugin(require("@11ty/eleventy-plugin-syntaxhighlight"));
 
+    // Configure BrowserSync to disable all reload functionality
+    eleventyConfig.setBrowserSyncConfig({
+        notify: false,  // Disable the notification overlay
+        ui: false,      // Disable the BrowserSync UI
+        ghostMode: false,  // Disable syncing across devices
+        open: false,    // Don't auto-open browser
+        reloadDelay: 0,
+        reloadDebounce: 0,
+        injectChanges: false,  // Don't inject CSS changes
+        codeSync: false,  // Disable code syncing
+        snippetOptions: {
+            rule: {
+                match: /<\/body>/i,
+                fn: function (snippet, match) {
+                    // Inject custom script to completely block reload dialogs
+                    return '<script>if(window.___browserSync___){window.___browserSync___.socket.on=function(){};}</script>' + match;
+                }
+            }
+        },
+        callbacks: {
+            ready: function (err, bs) {
+                // Disable reload confirmations at the source
+                bs.options.set('notify', false);
+                bs.options.set('reloadDelay', 0);
+            }
+        }
+    });
+
     return {
         dir: {
             input: "Lessons",
